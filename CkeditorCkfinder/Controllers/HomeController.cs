8af72202890355af1200e9a054050266a6a102b1
Mohammad <makbari.it@outlook.com>
2016-03-29 17:36:21 +0430

    init

---
+++ CkeditorCkfinder/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+
+namespace CkeditorCkfinder.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static string content = "";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(EditorVm editorVm)
+        {
+            content = editorVm.Content;
+            return View("Index",editorVm);
+        }
+
+        public ActionResult ViewPage()
+        {
+            var editorVm = new EditorVm()
+            {
+                Content = content
+            };
+            return View(editorVm);
+        }
+    }
+
+    public class EditorVm
+    {
+        [AllowHtml]
+        public string Content { get; set; }
+    }
+}
---
+++ CkeditorCkfinder/public/scripts/ckEditor.js
@@ -0,0 +1,49 @@
+﻿(function ($, customCkEditorModule) {
+
+    customCkEditorModule.init = function () {
+        for (name in CKEDITOR.instances) {
+            CKEDITOR.instances[name].destroy();
+        };
+        $(".wysiwyg").each(function (index, item) {
+
+            if (typeof CKEDITOR !== 'undefined') {
+                CKEDITOR.addCss('img {max-width:100%; height: auto;}');
+                var editor = CKEDITOR.replace('Content', {
+                    //extraPlugins: 'uploadimage,image2',
+                    //removePlugins: 'image',
+                    height: 350
+                });
+
+                // Just call CKFinder.setupCKEditor and pass the CKEditor instance as the first argument.
+                // The second parameter (optional), is the path for the CKFinder installation (default = "/ckfinder/").
+                CKFinder.setupCKEditor(editor, '/public/ckfinder');
+
+                // It is also possible to pass an object with selected CKFinder properties as a second argument.
+                // CKFinder.setupCKEditor( editor, { basePath : '../', skin : 'v1' } ) ;
+            } else {
+                document.getElementById('description').innerHTML = '<div class="tip-a tip-a-alert">This sample requires working Internet connection to load CKEditor from CDN.</div>'
+            }
+
+            //CKEDITOR.replace(id, {
+            //    language: "fa",
+            //    enterMode: CKEDITOR.ENTER_BR,
+            //    height: 500,
+            //    allowedContent: true,
+            //    filebrowserBrowseUrl: roxyFileman,
+            //    filebrowserImageBrowseUrl: roxyFileman + "?type=image",
+            //    removeDialogTabs: "link:upload;image:upload",
+            //    fullPage: false,
+            //    removePlugins: "divarea"
+            //});
+        });
+    }
+
+    var wireEvents = function () {
+
+    };
+
+    $(function () {
+        customCkEditorModule.init();
+        wireEvents();
+    });
+}(jQuery, customCkEditorModule = window.customCkEditorModule || {}));
